7ccf3d1898ceef1a012018b72495205cc282062c
Alan <adbishel@ncsu.edu>
2018-03-11 18:28:08 -0400

    Updated card class, added ranks and suits list

--- Project/card.py
+++ Project/card.py
@@ -1,25 +1,65 @@
-class Card:
-    number = 2
-    suit = 1
+from ranks import ranks
+from suits import suits
+
+class Card(object):
+
+    def __init__(self, rank, suit):
+        """
+        Init method for a card.
+        """
+        if rank in ranks and suit in suits:
+            self.rank = rank
+            self.suit = suit
+        else:
+            raise TypeError("Error while creating card")
     
-    def make_card():
-    #this is the constructor for card
-    
-    def getNumber():
-        return number
-    
-    def getSuit():
-        return suit
-    
-    def setNumber(i):
-        number = i
-        
-    def setSuit(i):
-        suit = i
-    
-    def equals(o):
-        #check if this card is equal to object o which is another card
-        #not sure if we need this, but it was in
-        
-    def compareTo(o):
-        #compare to another card (used to check if same suit or number)
+    def value(self):
+        """
+        Numerical value of a card
+        :return: The index of the cards rank in ranks
+        """
+        return ranks.index(self.rank)
+
+    def __eq__(self, card):
+        """
+        Equals operator
+        :return: True if two cards are equal.
+        """
+        if type(self) != type(card):
+            return False
+        return self.rank == card.rank and self.suit == card.suit
+
+    def __ne__(self, other):
+        """
+        Not-equals operator
+        :return: True if two cards are not equal.
+        """
+        return not self.__eq__(other)
+
+    def __gt__(self, card):
+        """
+        Greater-than operator
+        :return: True if self has a greater value than card
+        """
+        return self.value() > card.value()
+
+    def __lt__(self, card):
+        """
+        Less-than operator
+        :return: True if self has a lesser value than card
+        """
+        return self.value() < card.value()
+
+    def __ge__(self, card):
+        """
+        Greater-than, or equal, operator
+        :return: True if self has a greater than, or equal value to card
+        """
+        return self.value() >= card.value()
+
+    def __le__(self, card):
+        """
+        Less-than, or equal, operator
+        :return: True if self has a lesser or equal value than card
+        """
+        return self.value() <= card.value()
---
+++ Project/suits.py
@@ -0,0 +1,4 @@
+"""
+List of possible card ranks including C(lubs), D(iamonds) H(earts), and S(pades)
+"""
+suits = ["C", "D", "H", "S"]
